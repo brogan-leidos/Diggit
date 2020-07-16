73a7adef0dc2a5ea75b48639d646258ad8d413aa
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-16 10:09:27 -0400

    Update Object.js

--- biome/objects/Object.js
+++ biome/objects/Object.js
@@ -1,11 +1,13 @@
 export default class Object {
-  constructor(name="default", description="default discription", rarity=0) {
+  constructor(name="default", description="default discription", rarity=0, height=0, width=0) {
     this.Name = name;
     this.description = description;
     this.rarity = rarity;
+    this.height = height;
+    this.width = width;
   }
   
   getDimensions() {
-    return;
+    return [this.height, this.width];
   }
 }
