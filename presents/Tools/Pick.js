e8971eef6b89b4ed8aa0af3e9270d48a8018297c
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-16 09:19:24 -0400

    Update Pick.js

--- presents/Tools/Pick.js
+++ presents/Tools/Pick.js
@@ -1,3 +1,18 @@
-export class Pick {
+import Tool from '../Tool.js'
 
+export default class Pick extends Tool {
+  constructor() {
+    super();
+    this.Name = "Pick";
+    this.power = 1;
+    this.durability = -1;
+    
+    // Return an array of surrounding spots and the damage done to them
+    mineSpot(spot) {
+      return;
+    }
+    
+    
+  }
+  
 }
