aa81a3ed4e420b7f3da432b40d9c5be6df58e465
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-16 10:01:59 -0400

    Update Biome.js

--- biome/Biome.js
+++ biome/Biome.js
@@ -3,7 +3,7 @@ export default class Biome {
     this.temp = 0; 
   }
 
-  function getLargeObjects();
-  function getMedObjects();
-  function getSmallObjects();
+  getLargeObjects();
+  getMedObjects();
+  getSmallObjects();
 }
