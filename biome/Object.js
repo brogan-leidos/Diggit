61783696f130383a63dd2686dcb841cd93787cec
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-16 10:06:24 -0400

    Update Object.js

--- biome/Object.js
+++ biome/Object.js
@@ -2,6 +2,6 @@ export default class Object {
   constructor(name="default", description="default discription", rarity=0) {
     this.Name = name;
     this.description = description;
-    rarity = rarity;
+    this.rarity = rarity;
   }
 }
