69368a39e265b7054052b5db059e0bc58a5cbf9c
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-16 10:06:11 -0400

    Create Object.js

---
+++ biome/Object.js
@@ -0,0 +1,7 @@
+export default class Object {
+  constructor(name="default", description="default discription", rarity=0) {
+    this.Name = name;
+    this.description = description;
+    rarity = rarity;
+  }
+}
