e3ec1340c7adf3443bd0db59284cadc5c31c35f4
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-16 14:24:01 -0400

    Update GameGridSettings.js

--- engine/GameGridSettings.js
+++ engine/GameGridSettings.js
@@ -1,8 +1,8 @@
 export default class GameGridSettings() {
-  constructor(height, width, rarity) {
+  constructor(height, width, rarity, biome) {
     this.width = 0;
     this.height = 0;
     this.rarity = rarity;
-    this.biome = null;   
+    this.biome = biome;   
   }
 }
