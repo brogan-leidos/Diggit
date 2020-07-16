e26d25dc244812f0ae31b45035867459f4180ba2
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-16 09:15:51 -0400

    Update Weapon.js

--- presents/Weapon.js
+++ presents/Weapon.js
@@ -1,14 +1,13 @@
-export default class Weapon {
+export default class Tool {
     
-    constructor (name="", numdice=0, ddie="", dtype="", hit=0, dmg=0){
-        this.Name = name
-        this.NumDice = numdice
-        this.DamageDie = ddie        
-        this.DamageType = dtype
-        this.BonusToHit = hit
-        this.BonusToDmg = dmg
+    constructor (){
+        this.power = 0;
+        this.durability = 0;
+        
+        mineSpot(spot) {
+            return;
+        }
+        
     }
         
-//     def __str__ (self):
-//         return "{4}: +{0} to hit, ({1}) + {2} {3} damage".format(self.BonusToHit, self.DamageDie, self.BonusToDmg, self.DamageType, self.Name)
 }
