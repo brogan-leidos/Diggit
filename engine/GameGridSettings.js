2f97c1bca1a9883e52ad2918157a635083bd4bed
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-16 14:14:51 -0400

    Create GameGridSettings.js

---
+++ engine/GameGridSettings.js
@@ -0,0 +1,8 @@
+export default class GameGridSettings() {
+  constructor(height, width, rarity)
+    this.width = 0;
+    this.height = 0;
+    this.rarity = rarity;
+    this.biome = null;   
+  }
+}
