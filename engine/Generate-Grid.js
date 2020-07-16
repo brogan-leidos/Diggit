eaf04552ddc8439208968e69eaa2a66f3fe25587
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-16 09:25:21 -0400

    Update Generate-Grid.js

--- engine/Generate-Grid.js
+++ engine/Generate-Grid.js
@@ -1,3 +1,17 @@
+
+// Generates 2 layers of the grid
+// Layer 1: the underlying loot layer, this is where the objects go
+// Layer 2: The overlaying dirt layer. This determines how hard it is to dig down
 export function generateGrid(height=5, width=5) {
+  var grid = [];
+  
+  // Initialize the grid
+  for (var x = 0; x < width; x++) {
+    grid[x] = new Array();
+    for (var y = 0; y < height; y++) {
+       
+    }
+  }
+  
   return;
 }
