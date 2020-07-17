933ac14362e5c6e0da569ec00ce51c6a08b66d9a
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-17 07:36:14 -0400

    Update web.js

--- web.js
+++ web.js
@@ -8,16 +8,50 @@ var gameGrid = new GameGrid();
 
 export default () => {
     document.getElementById('generateButton').addEventListener('click', () => {
-        createGrid();
+        gameGrid = createGameGrid();
+        refreshGrid(gameGrid);
     });
     
 };
 
-function createGrid() {
+function createGameGrid() {
     var width = document.getElementById("width").value;
     var height = document.getElementById("height").value;
     var rarity = document.getElementById("rarity").value;
     
     gameGrid = GenerateGrid(gameGrid);
     gameGrid.settings = new GameGridSEttings(width, height, rarity, new Generic());
+    
+    return gameGrid;
+}
+
+function refreshGrid(grid) {
+    var gameSection = document.getElementById("gameSection");
+    gameSection.innerHTML = "";
+    
+    // Display upper grid and then lower grid
+    // Generate a table based on the given grids
+    
+    var htmlResult = "";
+    
+    for (var i=0; i < gameGrid.upperGrid.length; i++) {
+        hmtlResult += "<tr>";
+        for (var j=0; j < gameGrid.upperGrid[i].length; j++) {
+                htmlResult += "<td>" + gameGrid.upperGrid[i].toString() + "</td>"
+        }
+    }
+    
+    htmlResult += "Divider time";
+    
+    
+    for (var i=0; i < gameGrid.lowerGrid.length; i++) {
+        hmtlResult += "<tr>";
+        for (var j=0; j < gameGrid.lowerGrid[i].length; j++) {
+                htmlResult += "<td>" + gameGrid.lowerGrid[i].toString() + "</td>"
+        }
+    }
+    
+    gameSection.innerHTML = "";
+    gameSection.insertAdjacentHTML('beforeend', htmlResult);
+    return htmlResult;
 }
