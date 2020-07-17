d025f9e92902800b42ca9dec732600ae71a263c7
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-17 12:44:47 -0400

    Update Tool.js

--- presents/Tool.js
+++ presents/Tool.js
@@ -3,6 +3,7 @@ export default class Tool {
     constructor (){
         this.power = 0;
         this.durability = 0;
+        this.damage = 0;
         
         // Given a point (x,y) return all spots affected by the mine
         mineSpot(spot, height, width) {
