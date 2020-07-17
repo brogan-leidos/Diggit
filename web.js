18e096023fe0aa1999b9130bfe3ecab5692d4de7
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-17 13:11:59 -0400

    Update web.js

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ import GameGrid from './engine/GameGrid.js'
 import {generateGrid} from './engine/Generate-Grid.js'
 import GameGridSettings from './engine/GameGridSettings.js'
 import Generic from './biome/Generic.js'
-import Tool from './presents/tools/Tool.js'
-import Pick from './presents/tools/Pick.js'
+import Tool from './presents/Tools/Tool.js'
+import Pick from './presents/Tools/Pick.js'
 
 
 var gameGrid = new GameGrid();
