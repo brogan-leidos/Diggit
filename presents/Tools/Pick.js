ac79ef0492e75801f5ba57cbfd881bb3b54d7b76
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-27 14:52:31 -0400

    Update Pick.js

--- presents/Tools/Pick.js
+++ presents/Tools/Pick.js
@@ -6,12 +6,12 @@ export default class Pick extends Tool {
     this.Name = "Pick";
     this.power = 1;
     this.durability = -1;
-    this.damage = 5;
+    this.damage = 4;
   } 
   
   // Return an array of surrounding spots and the damage done to them
   getMinableSpots(x, y) {
-    return [[x, y, this.power],[x+1, y, this.power],[x-1, y, this.power],[x, y+1, this.power],[x, y-1, this.power]];
+    return [[x, y, this.power+1],[x+1, y, this.power],[x-1, y, this.power],[x, y+1, this.power],[x, y-1, this.power]];
   }
  
 }
