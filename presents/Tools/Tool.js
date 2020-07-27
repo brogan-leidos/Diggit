835caf4ea3332f45736a0093c1ae4fb56f3c175c
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-27 08:02:57 -0400

    Update Tool.js

--- presents/Tools/Tool.js
+++ presents/Tools/Tool.js
@@ -4,12 +4,11 @@ export default class Tool {
         this.power = 0;
         this.durability = 0;
         this.damage = 0;
-        
-        // Given a point (x,y) return all spots affected by the mine
-        mineSpot(spot, height, width) {
-            return;
-        }
-        
     }
-        
+    
+    // Given a point (x,y) return all spots affected by the mine
+    mineSpot(spot, height, width) {
+        return;
+    }
+              
 }
