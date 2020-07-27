2a15cdd46236ac801a03ece0c613ca68a94b45df
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-27 12:47:33 -0400

    Update Object.js

--- biome/objects/Object.js
+++ biome/objects/Object.js
@@ -4,12 +4,27 @@ export default class Object {
     this.Name = name;
     this.description = description;
     this.rarity = rarity;
+    
+    this.origin = [-1,-1];
     this.height = height;
     this.width = width;
+    
     this.Color = color;
+    
   }
   
   getDimensions() {
     return [this.height, this.width];
+  }
+  
+  
+  getOccupiedSpots() {
+    var ret = [];
+    for (var x=0; x < this.width; x++) {      
+      for (var y=0; y < this.height; y++) {
+        ret.push([this.origin[0]+x, this.origin[1]+y]);
+      }
+    }
+    return ret;
   }
 }
