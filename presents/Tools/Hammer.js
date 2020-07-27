c1caae77ca179163b61d1f5f4175ba97e0a3dc2c
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-27 09:32:12 -0400

    Create Hammer.js

---
+++ presents/Tools/Hammer.js
@@ -0,0 +1,18 @@
+import Tool from './Tool.js'
+
+export default class Hammer extends Tool {
+  constructor() {
+    super();
+    this.Name = "Hammer";
+    this.power = 2;
+    this.durability = -1;
+    this.damage = 15;
+  } 
+  
+  // Return an array of surrounding spots and the damage done to them
+  getMinableSpots(x, y) {
+    return [[x, y, this.power],[x+1, y, this.power],[x-1, y, this.power],[x, y+1, this.power],[x, y-1, this.power],
+            [x-1, y-1, this.power],[x+1, y+1, this.power],[x-1, y+1, this.power],[x+1, y-1, this.power]];
+  }
+ 
+}
