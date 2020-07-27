d37a265a73ec5005cde2e5cc7796c323bca881a2
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-27 09:04:40 -0400

    Update web.js

--- web.js
+++ web.js
@@ -159,8 +159,9 @@ function refreshHealthBar() {
     var bar = document.getElementsByClassName("healthBar")[0];
     var percentRemaining = gameGrid.healthRemaining / gameGrid.maxHealth * 100;
     bar.style.width = `${percentRemaining}%`;
+    bar.innerHTML = `${percentRemaining}%};
     
-    if (percentRemaining >= 60) { bar.styles.color = "green"; }
-    if (percentRemaining < 60 && percentRemaining > 30) { bar.styles.color = "yellow"; }
-    if (percentRemaining <= 30) { bar.styles.color = "red"; }        
+    if (percentRemaining >= 60) { bar.style.color = "green"; }
+    if (percentRemaining < 60 && percentRemaining > 30) { bar.style.color = "yellow"; }
+    if (percentRemaining <= 30) { bar.style.color = "red"; }        
 }
