0c86b8a7dfa7ac30d24d8ea8cae18e9bc905f537
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-28 09:32:49 -0400

    Update Support.js

--- presents/Consumables/Support.js
+++ presents/Consumables/Support.js
@@ -1,1 +1,14 @@
+import Tool from './Tool.js'
 
+export default class Support extends Tool {
+  constructor() {
+    super();
+    this.Name = "Support";
+    this.power = 20;
+  } 
+  
+  behavior(gameGrid) {
+    gameGrid.remainingHealth += this.power;
+  }  
+ 
+}
