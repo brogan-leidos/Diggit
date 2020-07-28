b39fde5cdf50bfb925be3fb62205cc2ad59c8f37
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-28 14:03:06 -0400

    Update Biome.js

--- biome/Biome.js
+++ biome/Biome.js
@@ -2,6 +2,10 @@ export default class Biome {
   constructor() {    
     this.Temperature = 0; 
     this.MaxHardness = 0;
+    
+    this.GridBorderColor = "#ddd";
+    this.GridBackgroundColor = "#eee";
+    
     this.PressurePointsEnabled = false;
     this.IceSheetsEnabled = false;
     this.LavaPlumesEnabled = false;
