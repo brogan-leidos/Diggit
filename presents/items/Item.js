f0fc3ad42198297e99ba3d42fbb27cb322315567
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-28 09:35:15 -0400

    Create Item.js

---
+++ presents/items/Item.js
@@ -0,0 +1,14 @@
+export default class Item {
+    
+    constructor () {
+      this.Name = "Default item name";
+      this.Description = "Default item description";
+      this.Power = 0;
+      this.Damage = 0;
+    }
+    
+    behavior(gameGrid) {
+      return;
+    }
+              
+}
