56e34348283afba6c58cd1c87df6d6a0d570aee2
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-28 07:37:10 -0400

    Update Sapphire.js

--- biome/objects/Sapphire.js
+++ biome/objects/Sapphire.js
@@ -2,6 +2,8 @@ import Object from './Object.js'
 
 export default class Sapphire extends Object {
   constructor() {
-    super("Sapphire", ); 
+    super("Sapphire", "A sparkling blue gem", .1, 1, 1, "#3063f0", "biome/objects/images/sapphire.png");
+    this.Value = 1000;
+    this.Price = 2000;    
   }
 }
