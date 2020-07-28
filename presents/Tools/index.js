5668162ed2c2bea2f5370dd4f310b691ffc6078d
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-28 13:06:44 -0400

    Create index.js

---
+++ presents/Tools/index.js
@@ -0,0 +1,5 @@
+export { default as Tool } from "./Tool.js";
+export { default as Pick } from "./Pick.js";
+export { default as Hammer } from "./Hammer.js";
+export { default as Drill } from "./Drill.js";
+export { default as Vaporizer } from "./Vaporizer.js";
