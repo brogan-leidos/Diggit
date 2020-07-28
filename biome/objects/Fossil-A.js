5de73e3bd8aa1ed0f73064cfd216b9c8a266ae18
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-28 07:47:13 -0400

    Create Fossil-A.js

---
+++ biome/objects/Fossil-A.js
@@ -0,0 +1,16 @@
+import Object from './Object.js'
+
+export default class Fossil-A extends Object {
+  constructor() {
+    // Name, description, rarity, width, height, color, image
+    super("Special Fossil", "A very old fossil", .3, -1, -1, "#3063f0");
+    this.Value = 1000;
+    this.Price = 2000;
+    
+    // x = origin, . = empty space, anything else = part of the object
+    this.Shape = [
+    [x][o][o][.]
+    [o][.][o][o]
+    [o][o][o][o]];
+  }
+}
