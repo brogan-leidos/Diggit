073c9cfcc7098fe321a3cfecca6330b2b265d0c5
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-28 08:10:06 -0400

    Update Fossil_A.js

--- biome/objects/Fossil_A.js
+++ biome/objects/Fossil_A.js
@@ -3,7 +3,7 @@ import Object from './Object.js'
 export default class Fossil_A extends Object {
   constructor() {
     // Name, description, rarity, width, height, color, image
-    super("Special Fossil", "A very old fossil", .3, -1, -1, "#3063f0");
+    super("Special Fossil A", "A very old fossil", .3, -1, -1, "#03ecfc");
     this.Value = 1000;
     this.Price = 2000;
     
