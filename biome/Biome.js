bebaa62fa9d7d65debb54917a10b96dca4ae718c
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-28 12:41:30 -0400

    Update Biome.js

--- biome/Biome.js
+++ biome/Biome.js
@@ -1,6 +1,11 @@
 export default class Biome {
   constructor() {    
-    this.temp = 0; 
+    this.Temperature = 0; 
+    this.MaxHardness = 0;
+    this.PressurePointsEnabled = false;
+    this.IceSheetsEnabled = false;
+    this.LavaPlumesEnabled = false;
+    
   }
 
   getLargeObjects() { return; }
