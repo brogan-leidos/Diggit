e9b6439af5fcb9bb1ba492e145a2411c6e70520f
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-29 15:19:17 -0400

    Update index.js

--- biome/objects/index.js
+++ biome/objects/index.js
@@ -1,3 +1,4 @@
 export { default as Loot } from "./Loot.js";
 export { default as Sapphire } from "./Sapphire.js";
 export { default as Fossil_A } from "./Fossil_A.js";
+export { default as Diamond } from "./Diamond.js";
