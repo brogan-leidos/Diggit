2252dbb62c930338ca821cd2e1bd3c3bd9938d88
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-29 10:31:18 -0400

    Update Fossil_A.js

--- biome/objects/Fossil_A.js
+++ biome/objects/Fossil_A.js
@@ -1,9 +1,9 @@
 import Loot from './Loot.js'
 
 export default class Fossil_A extends Loot {
-  constructor() {
+  constructor(rarity) {
     // Name, description, rarity, width, height, color, image
-    super("Special Fossil A", "A very old fossil", .3, -1, -1, "#03ecfc");
+    super("Special Fossil A", "A very old fossil", rarity, -1, -1, "#03ecfc");
     this.Value = 1000;
     this.Price = 2000;
     
