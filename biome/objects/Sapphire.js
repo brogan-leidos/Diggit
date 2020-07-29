85c8389754c6e8b220c00faab2d93a8ebeb77ba6
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-29 10:31:01 -0400

    Update Sapphire.js

--- biome/objects/Sapphire.js
+++ biome/objects/Sapphire.js
@@ -1,8 +1,8 @@
 import Loot from './Loot.js'
 
 export default class Sapphire extends Loot {
-  constructor() {
-    super("Sapphire", "A sparkling blue gem", .1, 1, 1, "#3063f0", "biome/objects/images/sapphire.png");
+  constructor(rarity) {
+    super("Sapphire", "A sparkling blue gem", rarity, 1, 1, "#3063f0", "biome/objects/images/sapphire.png");
     this.Value = 1000;
     this.Price = 2000;    
   }
