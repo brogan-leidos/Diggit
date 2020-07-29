8ade707f7530cb8efd682f67b7e7c4a89fc3ee8a
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-29 14:00:54 -0400

    Update Inventory.js

--- engine/Inventory.js
+++ engine/Inventory.js
@@ -6,4 +6,22 @@ export default class Inventory {
     this.availableItems = [];
     this.availableBiomes = [];
   }
+  
+  addToInventory(object) {
+    if (this.inventory[object.Name] === undefined) {
+      this.inventory[object.Name] = new Array();      
+    }
+    this.inventory[object.Name].push(object);
+  }
+  
+  removeFromInventory(object) {
+    if (this.inventory[object.Name] === undefined){
+      // Something very wrong has happened
+    }
+    var retItem = this.inventory[object.Name].pop();
+    if (this.inventory[object.Name].length == 0) {
+      this.inventory.delete(object.Name);
+    }
+    return retItem;
+  }
 }
