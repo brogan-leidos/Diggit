9ed1ece2d0dba017d5dcae0f92ea0ac27ccb4456
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-29 14:14:38 -0400

    Update Inventory.js

--- engine/Inventory.js
+++ engine/Inventory.js
@@ -11,15 +11,15 @@ export default class Inventory {
     if (this.inventory.has(object.Name) == false) {
       this.inventory.set(object.Name, new Array());      
     }
-    this.inventory[object.Name].push(object);
+    this.inventory.get(object.Name).push(object);
   }
   
   removeFromInventory(objectName) {
     if (this.inventory.has(objectName) == false){
       // Something very wrong has happened
     }
-    var retItem = this.inventory[objectName].pop();
-    if (this.inventory[objectName].length == 0) {
+    var retItem = this.inventory.get(objectName).pop();
+    if (this.inventory.get(objectName).length == 0) {
       this.inventory.delete(objectName);
     }
     return retItem;
