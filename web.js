032879dda976ad6dc2b675e9f7a94eed3acc240a
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-29 11:31:46 -0400

    Update web.js

--- web.js
+++ web.js
@@ -162,23 +162,20 @@ function highlightValidSpaces(spotId) {
     var highlightMemory = [];    
     
     if (highlightedSpots.length != 0) {
-        for (var i=0; i < highlightedSpots.length; i++) {
-            var spotToMemorize = document.getElementById(`${highlightedSpots[i][0]},${highlightedSpots[i][1]}`);
-            highlightMemory.push([highlightedSpots[i], spotToMemorize.style.borderColor]);
-//             highlightMemory = highlightedSpots;
-        }
+        highlightMemory = highlightedSpots;        
     }
 
     var potentialSpots = selectedTool.getMinableSpots(x,y);    
     highlightedSpots = potentialSpots.filter(a => a[0] < gameGrid.settings.width && a[0] >= 0 && a[1] < gameGrid.settings.height && a[1] >= 0);
     
     for (var i=0; i < highlightMemory.length; i++) {
-        var spotToDim = document.getElementById(`${highlightMemory[i][0][0]},${highlightMemory[i][0][1]}`);
-        spotToDim.style.borderColor = highlightMemory[i][1];
+        var spotToDim = document.getElementById(`${highlightMemory[i][0]},${highlightMemory[i][1]}`);
+        spotToDim.style.borderColor = highlightMemory[i][2];
     }
     
     for (var i=0; i < highlightedSpots.length; i++) {     
         var spotToLight = document.getElementById(`${highlightedSpots[i][0]},${highlightedSpots[i][1]}`);
+        highlightedSpots[i][2] = spotToLight.style.borderColor;
         spotToLight.style.borderColor = 'red';
     }
 
