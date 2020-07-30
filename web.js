85ae9570125f98e8e162c652a96fcf087cd1678c
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-30 14:32:59 -0400

    Update web.js

--- web.js
+++ web.js
@@ -5,6 +5,7 @@ import Player from './engine/Player.js'
 import { Tool, Pick, Hammer, Drill, Vaporizer, Shovel, Chisel } from './presents/Tools/index.js' 
 import Support from './presents/items/Support.js'
 import BiomeManager from './biome/BiomeManager.js'
+import { tintTextColor, tintBgColor, tintHex } from './utils/ColorUtil.js'
 
 
 var gameGrid = new GameGrid();
@@ -186,36 +187,36 @@ function drawUpperSpot(x,y) {
     return `<td id="${x},${y}" class="dirt" style="${styles}"> ${text} </td>`;
 }
 
-function tintTextColor(bgColor) {
-    var colorAvg = Math.floor((parseInt("0x" + bgColor.substr(1,2)) + parseInt("0x" + bgColor.substr(3,2)) + parseInt("0x" + bgColor.substr(5,2))) / 3);
-    if (colorAvg <= 125) {
-        return "white";
-    }
-    else {
-        return "black";
-    }
-}
+// function tintTextColor(bgColor) {
+//     var colorAvg = Math.floor((parseInt("0x" + bgColor.substr(1,2)) + parseInt("0x" + bgColor.substr(3,2)) + parseInt("0x" + bgColor.substr(5,2))) / 3);
+//     if (colorAvg <= 125) {
+//         return "white";
+//     }
+//     else {
+//         return "black";
+//     }
+// }
 
-function tintBgColor(bgColor, gridValue) {
-    if (bgColor.length == 4) {
-        bgColor = `#${bgColor[1]}${bgColor[1]}${bgColor[2]}${bgColor[2]}${bgColor[3]}${bgColor[3]}`;
-    }
-    var gridColorTint = gridValue * 20;
-    var red = tintHex(parseInt("0x" + bgColor.substr(1,2)), gridColorTint);
-    var green = tintHex(parseInt("0x" + bgColor.substr(3,2)), gridColorTint);
-    var blue =  tintHex(parseInt("0x" + bgColor.substr(5,2)), gridColorTint);
+// function tintBgColor(bgColor, gridValue) {
+//     if (bgColor.length == 4) {
+//         bgColor = `#${bgColor[1]}${bgColor[1]}${bgColor[2]}${bgColor[2]}${bgColor[3]}${bgColor[3]}`;
+//     }
+//     var gridColorTint = gridValue * 20;
+//     var red = tintHex(parseInt("0x" + bgColor.substr(1,2)), gridColorTint);
+//     var green = tintHex(parseInt("0x" + bgColor.substr(3,2)), gridColorTint);
+//     var blue =  tintHex(parseInt("0x" + bgColor.substr(5,2)), gridColorTint);
     
-    return `#${red}${green}${blue}`;
-}
+//     return `#${red}${green}${blue}`;
+// }
 
-function tintHex(hex, tintAmount) {
-    hex = Math.max(hex - tintAmount, 0);
-    var hexString = hex.toString(16);
-    if (hexString.length == 1){
-        hexString = "0" + hexString[0];   
-    }
-    return hexString;
-}
+// function tintHex(hex, tintAmount) {
+//     hex = Math.max(hex - tintAmount, 0);
+//     var hexString = hex.toString(16);
+//     if (hexString.length == 1){
+//         hexString = "0" + hexString[0];   
+//     }
+//     return hexString;
+// }
 
 function assignEventsToGrid() {
     var dirtList = document.getElementsByClassName("dirt");
