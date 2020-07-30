d3dcde7e963b36ae63cd5bd0ad1bb5dc95c6b885
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-30 07:35:24 -0400

    Update index.js

--- presents/Tools/index.js
+++ presents/Tools/index.js
@@ -3,3 +3,4 @@ export { default as Pick } from "./Pick.js";
 export { default as Hammer } from "./Hammer.js";
 export { default as Drill } from "./Drill.js";
 export { default as Vaporizer } from "./Vaporizer.js";
+export { default as Shovel } from "./Shovel.js";
