a41144688b1adbbc39c7af110fa67749d5dda03b
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-30 10:02:08 -0400

    Update Shovel.js

--- presents/Tools/Shovel.js
+++ presents/Tools/Shovel.js
@@ -9,11 +9,25 @@ export default class Shovel extends Tool {
     this.MaxDurability = 20;
     this.durability = this.MaxDurability;
     this.damage = 5;
+    this.Angle = 0;
   } 
   
   // Return an array of surrounding spots and the damage done to them
   getMinableSpots(x, y) {
-    return [[x, y, this.power],[x+1, y, this.power],[x, y+1, this.power],[x+1, y+1, this.power]];
+    var angles = [
+      [[x, y, this.power],[x+1, y, this.power],[x, y+1, this.power],[x+1, y+1, this.power]],
+      [[x, y, this.power],[x+1, y, this.power],[x, y-1, this.power],[x+1, y-1, this.power]],
+      [[x, y, this.power],[x-1, y, this.power],[x, y-1, this.power],[x-1, y-1, this.power]],
+      [[x, y, this.power],[x-1, y, this.power],[x, y+1, this.power],[x-1, y+1, this.power]]
+                 ];
+    return angles[this.Angle];
+  }
+  
+  rotateTool(sign) {    
+    this.Angle = sign == 1 ? this.Angle + 1 : this.Angle - 1;
+    this.Angle = this.Angle == 4 ? 0 : this.Angle;
+    this.Angle = this.Angle == -1 ? 3 : this.Angle;
+    
   }
  
 }
