ccc8d685f2b473d3aeb66d95e005cabeb772da9c
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-30 10:44:57 -0400

    Update BiomeManager.js

--- biome/BiomeManager.js
+++ biome/BiomeManager.js
@@ -3,10 +3,11 @@ import Forest from './Forest.js'
 import Ocean from './Ocean.js'
 import Desert from './Desert.js'
 import Volcano from './Volcano.js'
+import Glacier from './Glacier.js'
 
 export default class BiomeManager {
   constructor() {
-    this.Biomes = [new Generic(), new Forest(), new Ocean(), new Desert(), new Volcano()];
+    this.Biomes = [new Generic(), new Forest(), new Ocean(), new Desert(), new Volcano(), new Glacier()];
     this.selectedBiome = this.Biomes[0];
   }
 }
