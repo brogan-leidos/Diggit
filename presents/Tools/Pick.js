f27dfba8a85cc77fd7906fd590ba29565b2743a8
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-31 11:02:37 -0400

    Update Pick.js

--- presents/Tools/Pick.js
+++ presents/Tools/Pick.js
@@ -10,10 +10,20 @@ export default class Pick extends Tool {
     this.durability = this.MaxDurability;
     this.damage = 8;
   } 
-  
-  // Return an array of surrounding spots and the damage done to them
+    
   getMinableSpots(x, y) {
-    return [[x, y, this.power+1],[x+1, y, this.power],[x-1, y, this.power],[x, y+1, this.power],[x, y-1, this.power]];
+    var angles = [
+      [[x, y, this.power+1],[x+1, y, this.power],[x-1, y, this.power],[x, y+1, this.power],[x, y-1, this.power]],
+      [[x, y, this.power+1],[x+1, y+1, this.power],[x-1, y-1, this.power],[x-1, y+1, this.power],[x+1, y-1, this.power]]
+                 ];
+    return angles[this.Angle];
+  }
+  
+  rotateTool(sign) {    
+    this.Angle = sign == 1 ? this.Angle + 1 : this.Angle - 1;
+    this.Angle = this.Angle == 2 ? 0 : this.Angle;
+    this.Angle = this.Angle == -1 ? 1 : this.Angle;
+    
   }
  
 }
