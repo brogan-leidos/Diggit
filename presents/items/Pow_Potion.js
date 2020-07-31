6246610d3c9e38f7a350d846eb52131e1b2c9573
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-07-31 07:57:02 -0400

    Create Pow_Potion.js

---
+++ presents/items/Pow_Potion.js
@@ -0,0 +1,17 @@
+import Item from './Item.js'
+
+export default class Pow_Potion extends Item {
+    
+    constructor () {
+      this.Name = "Potion of Power";
+      this.Description = "Maximizes the power of your next few swings!";
+      this.Power = 0;
+      this.Damage = 0;
+      this.NumberRemaining = 1;
+    }
+    
+    behavior(gameGrid) {
+      gameGrid.alterPlayerStats(20, 0, 0);
+    }
+              
+}
