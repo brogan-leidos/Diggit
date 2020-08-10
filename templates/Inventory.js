9acb7a5314072c3b1778f3dbce21b5aaf72c169f
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-10 13:35:10 -0400

    Create Inventory.js

---
+++ templates/Inventory.js
@@ -0,0 +1,22 @@
+export const travelTemplate = `
+<div id="currentLocation">
+Current Location: FILLER-LOCATION
+</div>
+<div id="availableLocations">
+Available Locations: FILLER-AVAILABLE-LOCATIONS
+</div>
+`;
+
+export function getInventoryTemplate(player, biomeManager) {
+  var currentLocation = biomeManager.selectedBiome.Name;
+  var availableLocations = "";
+  for (var i=0; i < player.availableBiomes.length; i++) {
+    var biome = player.availableBiomes[i];
+    availableLocations += `<button id="Travel-${biome}">${biome}</button> 3 days away<br>`;
+  }
+  
+  var retValue = travelTemplate.replace("FILLER-LOCATION", currentLocation);
+  var retValue = retValue.replace("FILLER-AVAILABLE-LOCATIONS", availableLocations);
+  
+  return retValue;
+}
