d0ba3246ef78712adc1c3132488a6e5477bc8835
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-10 13:20:57 -0400

    Update index.js

--- biome/objects/index.js
+++ biome/objects/index.js
@@ -1,6 +1,4 @@
 export { default as Loot } from "./Loot.js";
 export { default as Fossil_A } from "./Fossil_A.js";
-export { default as Copper_Large } from "./Copper_Large.js";
-export { default as Copper_Medium } from "./Copper_Medium.js";
-export { default as Copper } from "./Copper.js";
 export * from './gems/index.js'
+export * from './metals/index.js'
