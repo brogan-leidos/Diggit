544b3f4d811a3c0fe42de26fd590cefeb570d8cf
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-10 13:08:00 -0400

    Update Generic.js

--- biome/Generic.js
+++ biome/Generic.js
@@ -1,7 +1,6 @@
 import Loot from './objects/Loot.js'
 import Biome from './Biome.js'
 import Fossil_A from './objects/Fossil_A.js'
-import Diamond from './objects/Diamond.js'
 
 export default class Generic extends Biome {
   constructor() {
@@ -18,7 +17,7 @@ export default class Generic extends Biome {
     objects.push(new Loot("Sapphire", "A sparkling blue gem",              .1, 1, 1, "#3063f0", "biome/objects/images/sapphire.png"));
     objects.push(new Loot("Emerald", "A sparkling green gem",              .1, 1, 1, "#0ef083"));
     objects.push(new Loot("Amythest", "A sparkling violet gem",            .1, 1, 1, "#9c56c4"));
-    objects.push(new Diamond(0.1));
+//     objects.push(new Diamond(0.1));
     objects.push(new Loot("Black Diamond", "An extremely rare, extremely precious gem", .005, 1, 1, "#d3d3d3"));    
     objects.push(new Loot("Copper Node", "A small node of copper",         .4, 1, 1, "#d4ad22"));
     objects.push(new Loot("Iron Node", "A small node of iron",             .3, 1, 1, "#949494"));
