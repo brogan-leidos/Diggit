be67f5c3147df81aad101476c31a54abc4f1ab6c
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-10 11:54:00 -0400

    Create Copper.js

---
+++ biome/objects/Copper.js
@@ -0,0 +1,11 @@
+import Loot from './Loot.js'
+
+export default class Copper extends Loot {
+  constructor(rarity) {
+    // Name, description, rarity, width, height, color, image
+    super("Copper Node", "A small node of copper", rarity, 1, 1, "#ffcc66");
+    this.Value = 1000;
+    this.Price = 2000;
+
+  }
+}
