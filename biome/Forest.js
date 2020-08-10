6ff337d50392660693411c08c6cc1a7feff169d2
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-10 11:28:31 -0400

    Update Forest.js

--- biome/Forest.js
+++ biome/Forest.js
@@ -1,5 +1,5 @@
 import Biome from './Biome.js'
-import { Loot, Sapphire, Fossil_A } from './objects/index.js';
+import { Loot, Sapphire, Fossil_A, Copper_Large, Copper_Medium } from './objects/index.js';
 
 export default class Forest extends Biome {
   constructor() {
@@ -21,7 +21,7 @@ export default class Forest extends Biome {
     objects.push(new Sapphire(.05));
     objects.push(new Loot("Emerald", "A sparkling green gem",              .05, 1, 1, "#0ef083"));
     objects.push(new Loot("Amythest", "A sparkling violet gem",            .05, 1, 1, "#9c56c4"));
-    objects.push(new Loot("Diamond", "A very precious gem",                .01, 1, 1, "#e3e3e3"));
+    objects.push(new Diamond(.01));
     objects.push(new Loot("Black Diamond", "An extremely rare, extremely precious gem", .001, 1, 1, "#d3d3d3"));    
     objects.push(new Loot("Copper Node", "A small node of copper",         .3, 1, 1, "#d4ad22"));
     objects.push(new Loot("Iron Node", "A small node of iron",             .2, 1, 1, "#949494"));
@@ -37,7 +37,7 @@ export default class Forest extends Biome {
   
   getMediumObjects() {
     var objects = [];
-    objects.push(new Loot("Med Copper Node", "A sizable node of copper", .4, 2, 3, "#d4ad22"));
+    objects.push(new Copper_Medium(.3));
     objects.push(new Loot("Med Iron Node", "A sizable node of iron", .3, 2, 3, "#949494"));
     objects.push(new Loot("Large Gold Nugget", "A large gold nugget", .1, 2, 2, "#ffea00"));
     objects.push(new Loot("Fossil", "A very old fossil", .3, 1, 2, "#61511e"));
@@ -48,7 +48,7 @@ export default class Forest extends Biome {
   
   getLargeObjects() {
     var objects = [];
-    objects.push(new Loot("Large Copper Node", "A large node of copper", .4, 3, 4, "#d4ad22"));
+    objects.push(new Copper_Large(.3));
     objects.push(new Loot("Large Iron Node", "A large node of iron", .3, 3, 4, "#949494"));
     objects.push(new Loot("Coal Vein", "A large vein of coal", .6, 3, 3, "#592b1d"));
     
