d1d81da7a065b2a56f6e4a6f5b3c08e9a2791caa
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-10 10:41:41 -0400

    Update Travel.js

--- templates/Travel.js
+++ templates/Travel.js
@@ -1,12 +1,25 @@
 export const travelTemplate = `
 
 <div id="currentLocation">
+Current Location: FILLER-LOCATION
 </div>
 
 <div id="availableLocations">
-</div>
-
-<div id="currentLocation">
+Available Locations: FILLER-AVAILABLE-LOCATIONS
 </div>
 
 `;
+
+export function getMapTemplate(player, biomeManager) {
+  var currentLocation = biomeManager.selectedBiome.Name;
+  var availableLocations = "";
+  for (var i=0; i < player.availableBiomes.length; i++) {
+    var biome = player.availableBiomes[i];
+    availableLocations += `<button id="Travel-${biome.Name}">${biome.Name}</button> 3 days away<br>`;
+  }
+  
+  var retValue = travelTemplate.replace("FILLER-LOCATION", currentLocation);
+  var retValue = retValue.replace("FILLER-AVAILABLE-LOCATIONS", availableLocations);
+  
+  return retValue;
+}
