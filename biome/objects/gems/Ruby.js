2ec232123471b4c42d3b95d266cf5f708c05c1eb
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-10 12:57:27 -0400

    Update Ruby.js

--- biome/objects/gems/Ruby.js
+++ biome/objects/gems/Ruby.js
@@ -2,7 +2,7 @@ import Loot from './Loot.js'
 
 export default class Ruby extends Loot {
   constructor(rarity) {
-    super("Ruby", "A sparkling red gem", rarity, 1, 1, "#ff0066");
+    super("Ruby", "A sparkling red gem", rarity, 1, 1, "#ff0066", "biome/objects/images/ruby.png");
     this.Value = 1000;
     this.Price = 2000;    
   }
