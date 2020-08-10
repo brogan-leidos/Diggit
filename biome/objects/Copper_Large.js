80392e19688e659950f9f2287a5291b03b0b0587
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-10 11:14:49 -0400

    Create Copper_Large.js

---
+++ biome/objects/Copper_Large.js
@@ -0,0 +1,18 @@
+import Loot from './Loot.js'
+
+export default class Copper_Large extends Loot {
+  constructor(rarity) {
+    // Name, description, rarity, width, height, color, image
+    super("Large Copper Node", "A large vein of copper", rarity, -1, -1, "#ffcc66");
+    this.Value = 1000;
+    this.Price = 2000;
+    this.Breakdown = [];
+    
+    // x = origin, . = empty space, anything else = part of the object
+    this.Shape = [
+    ["x","o","o"],
+    ["o","o","."],
+    [".","o","o"]
+    ["o","o","o"]];
+  }
+}
