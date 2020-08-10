c942581575768eba48876b569fc13f890187d08a
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-10 11:55:13 -0400

    Update Copper_Medium.js

--- biome/objects/Copper_Medium.js
+++ biome/objects/Copper_Medium.js
@@ -1,4 +1,5 @@
 import Loot from './Loot.js'
+import Copper from './Copper.js'
 
 export default class Copper_Medium extends Loot {
   constructor(rarity) {
@@ -6,7 +7,7 @@ export default class Copper_Medium extends Loot {
     super("Med Copper Node", "A sizable vein of copper", rarity, 2, 2, "#ffcc66");
     this.Value = 1000;
     this.Price = 2000;
-    this.Breakdown = [["Copper", 50]];
+    this.Breakdown = [[new Copper(), 50]];
     
   }
 }
