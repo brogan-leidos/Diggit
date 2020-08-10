5299f7e2f03652d04a3a20c8697043e45e6cfeb3
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-10 12:54:05 -0400

    Update and rename Amythest.js to Amethyst.js

---
+++ biome/objects/gems/Amethyst.js
@@ -0,0 +1,9 @@
+import Loot from './Loot.js'
+
+export default class Amethyst extends Loot {
+  constructor(rarity) {
+    super("Amethyst", "A sparkling voilet gem", rarity, 1, 1, "#ff00ff");
+    this.Value = 1000;
+    this.Price = 2000;    
+  }
+}
