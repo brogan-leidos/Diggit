930b3a2fc3b4323c9404ad3f1340d26aaf6af2aa
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-08-10 12:55:50 -0400

    Update index.js

--- biome/objects/gems/index.js
+++ biome/objects/gems/index.js
@@ -1,1 +1,6 @@
-export { Amythist } from './Amythest.js'
+export { Amethyst } from './Amethyst.js'
+export { Emerald } from './Emerald.js'
+export { Ruby } from './Ruby.js'
+export { Sapphire } from './Sapphire.js'
+export { Diamond } from './Diamond.js'
+
